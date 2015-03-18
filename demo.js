73b991ab9aa5e2c0df969bc1d0499e0785b6902c
Joe Minichino <joe.minichino@gmail.com>
2015-03-18 22:08:07 +0000

    basic stats complete

--- demo.js
+++ demo.js
@@ -1,11 +1,91 @@
 var loader = require('./index'),
-  filename = __dirname + '/plot_v2.csv';
+  filename = __dirname + '/plot_v2.csv',
+  loki = require('lokijs'),
+  records = new loki.Collection('records');
 
-loader.loadCsv(filename, { withHeaders: true, intoObjects: true }, function (data) {
-  
-  var filtered = data.data.filter(function (obj) {
-    return obj['1980\'s Average'] > 12812275;
+loader.loadCsv(filename, {
+  withHeaders: true,
+  intoObjects: true
+}, function (data) {
+  data.data.shift();
+  var years = ['2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'];
+
+  function parseValues(el) {
+    el['Average'] = parseInt(el['Average'], 10) || 0;
+    //el['2002'] = parseInt(el['2002'], 10) > 0 ? parseInt(el['2002'], 10) : el['Average'];
+    el['2003'] = parseInt(el['2003'], 10) | 0 > 0 ? parseInt(el['2003'], 10) | 0 : el['2000\'s Average'];
+    el['2004'] = parseInt(el['2004'], 10) | 0 > 0 ? parseInt(el['2004'], 10) | 0 : el['2000\'s Average'];
+    el['2005'] = parseInt(el['2005'], 10) | 0 > 0 ? parseInt(el['2005'], 10) | 0 : el['2000\'s Average'];
+    el['2006'] = parseInt(el['2006'], 10) | 0 > 0 ? parseInt(el['2006'], 10) | 0 : el['2000\'s Average'];
+    el['2007'] = parseInt(el['2007'], 10) | 0 > 0 ? parseInt(el['2007'], 10) | 0 : el['2000\'s Average'];
+    el['2008'] = parseInt(el['2008'], 10) | 0 > 0 ? parseInt(el['2008'], 10) | 0 : el['2000\'s Average'];
+    el['2009'] = parseInt(el['2009'], 10) | 0 > 0 ? parseInt(el['2009'], 10) | 0 : el['2000\'s Average'];
+    el['2010'] = parseInt(el['2010'], 10) | 0 > 0 ? parseInt(el['2010'], 10) | 0 : el['2000\'s Average'];
+    el['2011'] = parseInt(el['2011'], 10) | 0 > 0 ? parseInt(el['2011'], 10) | 0 : el['2000\'s Average'];
+    el['2012'] = parseInt(el['2012'], 10) | 0 > 0 ? parseInt(el['2012'], 10) | 0 : el['2000\'s Average'];
+    el['2013'] = parseInt(el['2013'], 10) | 0 > 0 ? parseInt(el['2013'], 10) | 0 : el['2000\'s Average'];
+    el['2014'] = parseInt(el['2014'], 10) | 0 > 0 ? parseInt(el['2014'], 10) | 0 : el['2000\'s Average'];
+    //el['2015'] = parseInt(el['2015'], 10) > 0 ? parseInt(el['2015'], 10) : el['Average'];
+    delete el['2002'];
+    delete el['2015'];
+    years.forEach(function (val) {
+      if (el[val] === -9999) {
+        el[val] = el['2000\'s Average'];
+      }
+    });
+    return el;
+  }
+
+  data.data.forEach(function (el) {
+    records.insert(parseValues(el));
   });
-  console.log(data.data)
-  console.log(filtered.length);
+
+
+  var sortFun = function (a, b) {
+    return a.value < b.value;
+  };
+  var maxima = years.map(function (obj) {
+    return records.maxRecord(obj);
+  }).sort(sortFun);
+
+  var alltimeMax = records.get(maxima[0].index);
+  console.log(alltimeMax);
+  var yearsData = years.map(function (o) {
+    return alltimeMax[o];
+  }).sort();
+  console.log(yearsData[yearsData.length - 1]);
+
+  var minima = years.map(function (obj) {
+    return records.minRecord(obj);
+  }).sort(sortFun);
+
+  console.log('All minimum value:\n', minima);
+  var alltimeMin = records.get(minima[minima.length - 1].index);
+  var minYearsData = years.map(function (o) {
+    return alltimeMin[o];
+  }).sort();
+  console.log(alltimeMin);
+  console.log(minYearsData[0]);
+  console.log(records.extractNumerical('2003').filter(function (n) {
+    return !(n === -9999 || isNaN(n));
+  }));
+  console.log(years.map(function (year) {
+    return {
+      year: year,
+      stdDev: records.stdDev(year)
+    };
+  }).sort(function (a, b) {
+    return a.stdDev < b.stdDev;
+  }));
+
+  console.log(years.map(function (year) {
+    return {
+      year: year,
+      average: records.avg(year)
+    }
+  }).sort(function (a, b) {
+    return a.average < b.average;
+  }));
+
+
 });
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 module.exports = (function () {
   var fs = require('fs');
 
+  function objectLoader(headers, data) {
+    var obj = {},
+      c = 0,
+      len = headers.length;
+    for (c; c < len; c += 1) {
+      obj[headers[c]] = data[c];
+    }
+    return obj;
+  }
+
+  function loadWithHeaders(headers) {
+    return function (data) {
+      return objectLoader(headers, data);
+    }
+  }
+
   return {
     loadCsv: function (file, options, callback) {
       fs.readFile(file, function (err, data) {
@@ -11,29 +27,21 @@ module.exports = (function () {
           i = 0,
           len = rows.length,
           opts = options || {},
-          csv = {};
+          csv = {},
+          localLoader;
+
         if (opts.withHeaders) {
           csv.headers = rows[0].split(',');
           // skip headers row in parsing
           i = 1;
         }
-        
-        function objectLoader(headers, data) {
-          var obj = {},
-            c = 0,
-            len = headers.length;
-          for (c; c < len; c += 1) {
-            obj[headers[c]] = data[c];
-          }
-          return obj;
-        }
 
-        var loader = opts.intoObjects ? objectLoader : function (headers, data) {
+        var loader = opts.intoObjects ? loadWithHeaders(csv.headers) : function (data) {
           return data;
-        }
+        };
 
         for (i; i < len; i += 1) {
-          rows[i] = loader(csv.headers, rows[i].split(','));
+          rows[i] = loader(rows[i].split(','));
         }
 
         csv.data = rows;
